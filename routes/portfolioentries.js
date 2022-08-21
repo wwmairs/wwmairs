4c5557fbae88b0d08f30fabf729ca0ac3a0ae2ad
Will <wwmairs@gmail.com>
2022-08-20 21:44:08 -0700

    moving files around; getting entry/edit page reading the data in correctly; building out entry/save route

--- routes/portfolioentries.js
+++ routes/portfolioentries.js
@@ -5,41 +5,83 @@ const sequelize = require("../sequelize");
 const { onlyWill } = require("../middleware.js");
 
 function getEntryByID(req, res) {
-	sequelize.models.PortfolioEntry.findOne({ where: { id: req.params.id},
-				       									  					include: sequelize.models.Photo })
+	fetchWithPhotosByID(req.params.id)
 		.then(portfolioEntry => {
-			res.render("entry", { entry: portfolioEntry });
+			res.render("entry/view", { entry: portfolioEntry });
 		});
+}
+
+function edit(req, res) {
+	fetchWithPhotosByID(req.params.id)
+		.then(portfolioEntry => {
+			res.render("entry/edit", { entry: portfolioEntry });
+		});
+}
+
+function fetchWithPhotosByID(id) {
+	return sequelize.models.PortfolioEntry.findOne({ where: { id: id},
+						          									  				 include: sequelize.models.Photo });
 }
 
 function getEntries(req, res) {
 	sequelize.models.PortfolioEntry.findAll({ include: sequelize.models.Photo,
 																						order: [["date", "DESC"]] })
 		.then((entries) => {
-			res.render("entries", { entries: entries, upload: req.session.isWill });
+			res.render("entry/all", { entries: entries, upload: req.session.isWill });
 		});
 }
 
 function saveEntry(req, res) {
-	var photos = [];
-	req.files.map(file => {
-		photos.push({
-			filename: file.filename,
-			originalname: file.originalname,
-			path: file.path
-		});
-	});
-	
-	sequelize.models.PortfolioEntry.create({
+	var portfolioEntry = {
 		name: req.body.name,
 		date: req.body.date,
 		description: req.body.description,
 		link: req.body.link,
-		Photos: photos
-	}, { 
+		Photos: extractPhotosIfAny(req)
+	};
+
+	var includePhotos = {
 		include: [sequelize.models.Photo]
-	});
+	}
+
+	if (!req.body.id) {
+		sequelize.models.PortfolioEntry.create(portfolioEntry, includePhotos);
+	} else {
+		var filter = {
+			where: {
+				id: req.body.id
+			}, 
+			include: [sequelize.models.Photo]
+		};
+
+		sequelize.models.PortfolioEntry.findOne(filter).then((oldEntry) => {
+			if (oldEntry) {
+				oldEntry.update(portfolioEntry, includePhotos);
+			} else {
+				throw new Error("no entry with that id to update");
+			}
+		});
+		// look for the id
+		// if match, update
+		// else just create
+	}
 	res.redirect("/entries");
+}
+
+function extractPhotosIfAny(req) {
+	var photos = [];
+	
+	if (req.files) {
+		req.files.map(file => {
+			photos.push({
+				filename: file.filename,
+				originalname: file.originalname,
+				path: file.path
+			});
+		});
+	}
+
+	return photos;
 }
 
 function defineRoutes(app) {
@@ -48,12 +90,13 @@ function defineRoutes(app) {
 	
 	app.get("/entries", getEntries);
 	
-	app.get("/upload", onlyWill, (req, res) => {
-		res.render("upload");
+	app.post("/entry/save", onlyWill, upload.array("photo"), saveEntry);
+	
+	app.get("/entry/create", onlyWill, (req, res) => {
+		res.render("entry/create");
 	});
-	
-	app.post("/upload", onlyWill, upload.array("photo"), saveEntry);
-	
+
+	app.get("/entry/edit/:id", edit);
 
 }
 
